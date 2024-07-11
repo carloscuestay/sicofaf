4ebcbe09449efa4c7815d5b3e1b6e9de40c14e6c
Isaias Perez <isapzmz@gmail.com>
2024-07-11 14:01:58 -0500

    ajuste api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   name: 'development',
   //path: '',
-  path: 'https://localhost:7162/api',
+  path: 'http://172.210.178.254/api',
   JWT_TOKEN: 'JWT_TOKEN',
   USER_INFO: 'USER_INFO',
   PERFILES_COMISARIAS: 'PERFILES_COMISARIAS',
